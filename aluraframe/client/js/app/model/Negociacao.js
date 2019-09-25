a287e79f23d522e3d2118916ddd33473bcd9fbec
Marcio Alexandre Freire Sindeaux <sindeaux1999@hotmail.com>
2019-09-25 20:34:26 -0300

    Aula 02: Especificando uma Negociação
    Commit das alterações realizadas no vídeo 03

--- aluraframe/client/js/app/model/Negociacao.js
+++ aluraframe/client/js/app/model/Negociacao.js
@@ -1,10 +1,13 @@
 class Negociacao{
 
-    constructor(){
-        
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0;
+    constructor(data, quantidade, valor){
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+
+    getVolume(){
+        return this.quantidade * this.valor;
     }
 
 }
