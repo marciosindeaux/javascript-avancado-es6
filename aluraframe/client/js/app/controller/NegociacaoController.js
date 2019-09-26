86b87324638428a202b847f8d51e334a71e38b38
Márcio Alexandre Freire Sindeaux <sindeaux1999@hotmail.com>
2019-09-26 01:00:33 -0300

    Aula 03:  A ligação entre as ações do usuário e o modelo
    Commit das alterações realizadas no vídeo 02

--- aluraframe/client/js/app/controller/NegociacaoController.js
+++ aluraframe/client/js/app/controller/NegociacaoController.js
@@ -3,6 +3,14 @@ class NegociacaoController{
 
     adiciona(event){
         event.preventDefault();
-        alert("Chamei ação no controller");
+
+        let select = document.querySelector.bind(document);
+
+        let inputData = select('#data');
+        let inputQuantidade = select('#quantidade');
+        let inputValor = select('#valor');
+
+        console.log(inputQuantidade.value);
+        console.log(inputValor.value);
     }
 }
